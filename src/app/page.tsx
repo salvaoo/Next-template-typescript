806f75acb8d42ab494674e1ff780169f98e6a346
salvaoo <salvadorgilrosales@outlook.es>
2023-06-26 16:59:29 +0100

    feat: Update the packages and ui/shadcn
    - Update the all packages.
    - Update the ui/shadcn components with lastest changes.

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -3,11 +3,11 @@ import Link from "next/link"
 import { siteConfig } from "@/config/site"
 import { buttonVariants } from "@/components/ui/button"
 
-export default function IndexPage() {
+export default function Home() {
   return (
     <section className="container grid items-center gap-6 pb-8 pt-6 md:py-10">
       <div className="flex max-w-[980px] flex-col items-start gap-2">
-        <h1 className="text-3xl font-extrabold leading-tight tracking-tighter sm:text-3xl md:text-5xl lg:text-6xl">
+        <h1 className="text-3xl font-extrabold leading-snug tracking-tighter sm:text-3xl md:text-5xl lg:text-6xl">
           {siteConfig.description}
         </h1>
         <p className="max-w-[700px] text-lg text-muted-foreground sm:text-xl">
